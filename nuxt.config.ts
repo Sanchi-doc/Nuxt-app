1c1ba3328f668f05577f0184bf940b4df768b57a
i.perekhodchenko <i.perekhodchenko@favbet.tech>
2024-08-16 10:10:07 +0300

    add log 6

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
         username: 'string'
       },
       token: {
-        signInResponseTokenPointer: 'token',
+        signInResponseTokenPointer: '/token',
         type: 'Bearer',
         cookieName: 'auth.token',
         headerName: 'Authorization',
