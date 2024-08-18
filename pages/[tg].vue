a4effd5e5dd88e2673a578f4b80f5b7aed4ee87a
i.perekhodchenko <i.perekhodchenko@favbet.tech>
2024-08-18 20:18:28 +0300

    add log 22

--- pages/[tg].vue
+++ pages/[tg].vue
@@ -17,7 +17,7 @@ const params = { tgId, username, password, email }
 const signup = async () => {
   console.log('Sign up:', params);
   try {
-    const res = await signUp({ params })
+    const res = await signUp(params)
     console.log('after sign up:', res)
   } catch (error) {
     console.error('Sign up error:', error)
